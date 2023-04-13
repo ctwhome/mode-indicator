af05b6683ebafd80cf94898b8e22ae217a8e32da
Jesús García <ctw@ctwhome.com>
2023-04-13 15:33:01 +0200

    add ear icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,6 +16,7 @@
 
 	let comment =''
 	let stress =''
+	let ear = false
 	let textarea
 	let modes = [];
 	onMount(async () => {
@@ -30,7 +31,8 @@
 			const newMode ={
 			    date: Timestamp.now(),
 			    comment,
-			    stress
+			    stress,
+					ear,
 			  }
 
 			try {
@@ -52,6 +54,7 @@
 		function resetForm() {
 			comment =''
 			stress =''
+			ear = false
 		}
 </script>
 
@@ -80,6 +83,7 @@
 
 
 <!-- Put this part before </body> tag -->
+
 <input type="checkbox" bind:checked={modal}  class="modal-toggle" />
 <div class="modal">
   <div class="modal-box bg-base-200">
@@ -90,7 +94,15 @@
 			<button class={`btn btn-circle bg-purple-500 hover:bg-purple-600 borner-none border-gray-300 ${stress ==='purple' && 'border-8'}`} on:click={()=>{stress='purple', textarea.focus()}}></button>
 			<button class={`btn btn-circle bg-yellow-400 hover:bg-yellow-600 borner-none border-gray-300 ${stress ==='yellow' && 'border-8'}`} on:click={()=>{stress='yellow', textarea.focus()}}></button>
 			<button class={`btn btn-circle bg-green-400 hover:bg-green-600 borner-none border-gray-300 ${stress ==='green' && 'border-8'}`} on:click={()=>{stress='green', textarea.focus()}}></button>
+			<div class="flex-1"></div>
+
+			<!-- Ear button -->
+			<button class={`btn btn-circle ${ear ===true ? 'border-4 bg-red-500':'bg-red-300'}  hover:bg-red-600 borner-none border-gray-300 `} on:click={()=>{ear = !ear, textarea.focus()}}>
+				<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M17 22q-1.55 0-2.538-.775T12.95 18.95q-.425-1.25-.812-1.75t-1.788-1.6Q8.8 14.35 7.9 12.775T7 9q0-2.975 2.013-4.987T14 2q2.975 0 4.988 2.013T21 9h-2q0-2.125-1.438-3.563T14 4q-2.125 0-3.563 1.438T9 9q0 1.7.675 2.9t1.925 2.15q1.3.95 2.025 1.85t1.075 1.95q.35 1.1.838 1.625T17 20q.825 0 1.413-.588T19 18h2q0 1.65-1.175 2.825T17 22ZM6.2 16.75q-1.475-1.5-2.338-3.488T3 9q0-2.3.863-4.3T6.2 1.2l1.45 1.4Q6.4 3.85 5.7 5.488T5 9q0 1.85.7 3.475t1.95 2.875l-1.45 1.4ZM14 11.5q-1.05 0-1.775-.738T11.5 9q0-1.05.725-1.775T14 6.5q1.05 0 1.775.725T16.5 9q0 1.025-.725 1.763T14 11.5Z"/></svg>
+			</button>
+
 		</div>
+
 
 
 		<br>
@@ -109,10 +121,26 @@
     <li class="flex gap-2 align-center text-md hover:bg-base-100 rounded">
 			<div class={`-translate-y-4 circle ${mode.stress === 'purple' && 'bg-purple-500'} ${mode.stress === 'yellow' && 'bg-yellow-500'} ${mode.stress === 'green' && 'bg-green-500'} w-10 h-10 rounded-xl`}></div>
 			<div>
-				<div class={`${!mode.comment && 'opacity-50 italic'} text-[1rem]`}>{mode.comment || "no comment"}</div>
+				<div class="flex gap-3 items-center">
+
+					{#if (mode.ear)}
+					<span class={`flex items-center justify-center rounded-full w-5 h-5 text-white bg-red-600 borner-none flex`} on:click={()=>{ear = !ear, textarea.focus()}}>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M17 22q-1.55 0-2.538-.775T12.95 18.95q-.425-1.25-.812-1.75t-1.788-1.6Q8.8 14.35 7.9 12.775T7 9q0-2.975 2.013-4.987T14 2q2.975 0 4.988 2.013T21 9h-2q0-2.125-1.438-3.563T14 4q-2.125 0-3.563 1.438T9 9q0 1.7.675 2.9t1.925 2.15q1.3.95 2.025 1.85t1.075 1.95q.35 1.1.838 1.625T17 20q.825 0 1.413-.588T19 18h2q0 1.65-1.175 2.825T17 22ZM6.2 16.75q-1.475-1.5-2.338-3.488T3 9q0-2.3.863-4.3T6.2 1.2l1.45 1.4Q6.4 3.85 5.7 5.488T5 9q0 1.85.7 3.475t1.95 2.875l-1.45 1.4ZM14 11.5q-1.05 0-1.775-.738T11.5 9q0-1.05.725-1.775T14 6.5q1.05 0 1.775.725T16.5 9q0 1.025-.725 1.763T14 11.5Z"/></svg>
+					</span>
+					{/if}
+					<div class={`${!mode.comment && 'opacity-50 italic'} text-[1rem]`}>{mode.comment || "no comment"}</div>
+				</div>
 				<div class="text-sm">{ mode.date?.toDate().getDate() } { mode.date?.toDate().toLocaleString('default', { month: 'long' })}, { day[mode.date?.toDate().getDay()]}
 					om
-					{ mode.date?.toDate().getHours()}:{String(mode.date?.toDate().getMinutes()).padStart(2, "0")} <button on:click={()=>deleteFeeling(mode.id)}><span class="ml-4">🗑️</span></button></div>
+
+					{ mode.date?.toDate().getHours()}:{String(mode.date?.toDate().getMinutes()).padStart(2, "0")}
+
+					<button on:click={()=>deleteFeeling(mode.id)}><span class="ml-4">🗑️</span></button>
+
+
+				</div>
+
+
 			</div>
 		</li>
   {/each}
